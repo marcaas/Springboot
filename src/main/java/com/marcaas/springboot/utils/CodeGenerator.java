570c3c59b4fcb2ad71aac5b2ebfdb32d88056e96
marcaas <t13274515361@gmail.com>
2023-04-07 09:05:13 +0800

    '2023.04.07'

--- src/main/java/com/marcaas/springboot/utils/CodeGenerator.java
+++ src/main/java/com/marcaas/springboot/utils/CodeGenerator.java
@@ -12,7 +12,7 @@ import java.util.Collections;
 
 public class CodeGenerator {
 
-    public static  void main(String[] args) {
+    public static void main(String[] args) {
         generate();
     }
 
